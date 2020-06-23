bcca7516aafe8793e138e81ba94fa255d01275d0
Pablo Sánchez <pablodkn@gmail.com>
2020-06-23 17:26:18 -0400

    Still don't know

---
+++ commands/1step.js
@@ -0,0 +1,49 @@
+exports.run = (client, message, Discord, fs) => {
+    savebase = JSON.parse(require ("../temp Json/jsonsavebasefile.json"));
+    var playerNamejson = message.author.username + ".json";
+    var path = "saves/" + playerNamejson;
+    fs.writeFile(path, savebaseobject, function (err) {   // JSON maker
+        if (err) throw err;
+        console.log('JSON created"!');
+
+    message.channel.send("Welcome "+ message.author.username +"! Select your language, please: English or spanish?")
+    .then(() => {
+        message.channel.awaitMessages(response => response.content, {
+          max: 1,
+          time: 60000,
+          errors: ['time'],
+        })
+        .then((collected) => {
+          switch (collected.first().content) {
+            case "esp":
+            case "Español":
+            case "español":
+            case "spa":
+            case "Spa":
+            case "spanish":
+            case "Spanish":
+            message.channel.send(`¡Muchas muchas gracias!`);
+            var language = "spanish";
+            savedbase.language = language
+            break;
+
+            case "Ing":
+            case "ing":
+            case "Eng":
+            case "eng":
+            case "inglés":
+            case "Inglés":
+            case "Ingles":
+            message.channel.send(`Thank you so much! `);
+            var language = "english";
+            savebasedobject.language = language
+            break;
+            default:
+            message.channel.send(`Restart. Can't read that.`);
+          }})
+          .catch(() => {
+            message.channel.send("Out of time. Try again.");
+          });
+      })
+})
+};
